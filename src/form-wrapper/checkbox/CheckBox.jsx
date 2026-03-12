1a027087dbca0a907367ba0fa99230b0839a683c
kiran-kharsel <kirankharsel@gmail.com>
2026-03-12 11:18:43 +0530

    feat: create text-field and checkbox component

---
+++ src/form-wrapper/checkbox/CheckBox.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './style.css'
+
+function CheckBox() {
+  return (
+    <input type="checkbox" />
+  )
+}
+
+export default CheckBox
---
+++ src/form-wrapper/text-field/TextField.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './style.css'
+
+function TextField() {
+  return (
+    <input type="text" />
+  )
+}
+
+export default TextField
